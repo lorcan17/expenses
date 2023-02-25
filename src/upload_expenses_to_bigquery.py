dc2879b920ef53a9a7a3f10fb3c5fcf136980e61
Lorcan Travers <ltravers92@gmail.com>
2023-02-25 09:54:16 -0800

    Reduce pull to quarterly and ensure all dates

--- src/upload_expenses_to_bigquery.py
+++ src/upload_expenses_to_bigquery.py
@@ -3,22 +3,23 @@ from functions import google_funcs, sw_funcs
 
 s = sw_funcs.sw_connect_api()
 group_id = sw_funcs.sw_group_id(s,"Everyday spEnding")
-
+date_from_list = pd.date_range(
+        start='2019-12-01',
+        end = pd.to_datetime('today'),
+        freq='Q'
+        )
+date_to_list = pd.date_range(
+        start='2020-04-01',
+        end = pd.to_datetime('today') + pd.to_timedelta(90,'D'),
+        freq='QS')
 
 dates = pd.DataFrame(
-    {'date_from':
-        pd.date_range(
-        start='1/1/2020',
-        end = pd.to_datetime('today'),
-        freq='YS'
-        ),
-    'date_to':
-        pd.date_range(
-        start='1/1/2020',
-        end = pd.to_datetime('today') + pd.to_timedelta(365,'D'),
-        freq='Y')
-    })
-
+    {
+        'date_from': date_from_list,
+        'date_to': date_to_list
+    }
+    )
+print(dates)
 for ind in dates.index:
     date_from = dates['date_from'][ind]
     date_to = dates['date_to'][ind]
@@ -27,7 +28,6 @@ for ind in dates.index:
         date_before = date_to,
         date_after = date_from
         )
-    
     if ind == 0:
         WRITE_DISPOSITION = 'WRITE_TRUNCATE'
     else:
